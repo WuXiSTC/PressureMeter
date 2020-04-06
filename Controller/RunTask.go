4ab0e98109688857598f14d5d6c9d63cf3d1fc06
yindaheng98 <yindaheng98@163.com>
2020-04-06 21:29:23 +0800

    设计图更新

--- Controller/RunTask.go
+++ Controller/RunTask.go
@@ -36,16 +36,25 @@ func StartTask(ctx iris.Context) {
 //返回停止是否成功和错误信息
 func StopTask(ctx iris.Context) {
 	taskId := ctx.Params().Get("id")
-	if err := Model.TaskList.Stop(taskId); err != nil {
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- Model.TaskList.Stop(taskId)
+	}()
+	select {
+	case err := <-errChan:
+		if err == nil {
+			responseMsg(ctx, iris.Map{"ok": true, "message": "停止成功"})
+			return
+		}
 		switch err.Error() {
 		case "not exists":
 			ctx.StatusCode(iris.StatusNotFound)
 		default:
 			responseMsg(ctx, iris.Map{"ok": false, "message": err.Error()})
 		}
-		return
+	case <-time.After(5e8): //如果停止任务耗时过长就尽快返回
+		responseMsg(ctx, iris.Map{"ok": true, "message": "停止请求已提交"})
 	}
-	responseMsg(ctx, iris.Map{"ok": true, "message": "停止成功"})
 }
 
 func GetState(ctx iris.Context) {
